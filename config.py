ac2437fbcf231249dfe20f6931e65406a31e78ee
Kvel4 <39651844+Kvel4@users.noreply.github.com>
2018-05-27 19:30:18 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+token = '534489748:AAHshR88itmKmN_5HSQZeMdZ1CY3zTz60aI'
+
+db_file = "database.db"
+
+class States():
+    start = '0'
+    name = '1'
+    event = '2'
+    city = '3'
+    age = '4'
+    hobby = '5'
+    questionary_end = '6'
