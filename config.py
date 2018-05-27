c11abae8b551dfb6b889a77960572c137269256d
Kvel4 <39651844+Kvel4@users.noreply.github.com>
2018-05-27 19:30:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,3 @@
 token = '534489748:AAHshR88itmKmN_5HSQZeMdZ1CY3zTz60aI'
 
 db_file = "database.db"
-
-class States():
-    start = '0'
-    name = '1'
-    event = '2'
-    city = '3'
-    age = '4'
-    hobby = '5'
-    questionary_end = '6'
